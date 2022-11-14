9402edcacd773418d42f3f4c4f5266fa28d97166
mynah <113277318+mynahshetty11@users.noreply.github.com>
2022-11-14 13:45:29 -0800

    Update Chart2.R

--- docs/Chart2.R
+++ docs/Chart2.R
@@ -12,3 +12,19 @@ group_by(Year = Year -Year %% 5) %>%
 View(diarrheal_death)
 
 plot(diarrheal_death$Year, diarrheal_death$Number.of.deaths)
+
+#Handwashing data
+handwash <- read.csv("project-mynahshetty11/data/data_handwash.csv")
+
+#Rename columns for handwash
+handwash_recent <- slice(handwash,3:n())
+names(handwash_recent) <-  paste0(names(handwash_recent), ",") %>%
+                               str_replace("\\.1,", ", Urban") %>%
+                          str_replace("\\.2,", ", Rural") %>%
+                          str_replace("\\,$", ", Both")
+
+#Convert to rows
+handwash_recent <- rename(handwash_recent, Countries = "X, Both") %>%
+                      gather("Year_and_type", "Percent", -Countries) %>%
+                    separate("Year_and_type", into = c("Year","Urban,Rural, or Both"), sep = ", ")
+                      
