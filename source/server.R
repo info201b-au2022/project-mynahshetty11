5fb50b80b0d1955f68be0679bf39e393c700a3b6
rylouie <114609375+rylouie@users.noreply.github.com>
2022-12-08 11:36:00 -0800

    Update server.R

--- source/server.R
+++ source/server.R
@@ -26,13 +26,15 @@ server <- shinyServer(
       req(input$Year)
       react_df <- merge1 %>% filter(Year %in% input$Year) %>% group_by(country_type) %>% summarise(Percent = (mean((Percent)))) 
     })
-    output$chart <- renderPlot({
-      ggplot(data = merge_df(), aes_string(x = "country_type", y = "Percent", fill = 'country_type')) + 
+    output$chart <- renderPlotly({
+      ggplotly(
+        ggplot(data = merge_df(), aes_string(x = "country_type", y = "Percent", fill = 'country_type')) + 
         geom_bar(stat = "identity", width = 0.5) + labs(x = "Country Type", y = "Percent of Countries with Access to Handwashing", fill = "Country Type") +  
         labs(title = "Percent of Handwashing Availibility in types of Countries Per Year",
              caption = "Caption: This depicts the average proportion per year of 
       handwashing in countries based on their classification of their living quality. 
       This was calculated based on what percentage the country was in poverty." )
+      )
     })
     
     
